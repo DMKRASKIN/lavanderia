655be0404fc2f1e8acbff5d1b754a400e05f8c3d
DMKRASKIN <123311323+DMKRASKIN@users.noreply.github.com>
2023-11-14 09:45:34 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,12 @@
+(function($){
+    $(document).ready(function(){
+    
+    $("#cssmenu").menumaker({
+        title: "NAVEGUE POR AQUI",
+        breakpoint: 768,
+        format: "multitoggle"
+    });
+    
+    });
+    })(jQuery);
+    
